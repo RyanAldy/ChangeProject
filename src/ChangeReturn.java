5fad3cac1dc4f4da5c52253f04409e0ac5f261a4
Ryan Alderson <ryanalderson91@gmail.com>
2017-10-17 17:16:20 +0100

    Initial commit

---
+++ src/ChangeReturn.java
@@ -0,0 +1,34 @@
+
+public class ChangeReturn {
+
+	public void Details (int Amount, int Paid) {
+		
+		float change = Paid - Amount;
+		
+		System.out.println("Change = " + change);
+		
+		int fiftyNote = 0;
+		int TwentyNote = 0;
+		int TenNote = 0;
+		int FiveNote = 0;
+		int TwoCoin = 0;
+		int OneCoin = 0;
+		int FiftyPence = 0;
+		int TwentyPence = 0;
+		int TenPence = 0;
+		int FivePence = 0;
+		int TwoPence = 0;
+		int OnePence = 0;
+		
+		
+		
+
+		
+		
+		
+
+		
+		
+	}
+	
+}
---
+++ src/ChangeStart.java
@@ -0,0 +1,15 @@
+
+public class ChangeStart {
+
+	public static void main(String[] args) {
+		
+		ChangeReturn x = new ChangeReturn();
+		
+		x.Details(1, 30);
+		
+		System.out.println(29.0 / 20);
+		System.out.println(3%2);
+
+	}
+
+}
